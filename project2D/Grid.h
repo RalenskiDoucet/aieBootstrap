0dae3d068fefc0b1fe637ee5fb63b983b749615e
Ralenski Doucet <Ralenskid@gmail.com>
2017-12-06 12:24:42 -0600

    Finshed Documentation

--- project2D/Grid.h
+++ project2D/Grid.h
@@ -7,5 +7,5 @@ public:
 	const int mapwidth = 1280;//sets a constant measurement for the width of the game board.
 	const int mapheight = 1280;//sets a constant measurement for the heigth of the game board.
 	int Grid(int Cols, int Rows);//a function that assigns that the grid is made of Colmons and Rows.
-	int DrawGrid();//a function that draws an ivisiable grid that is placed on the inside of the game board and sets bo
+	int DrawGrid();//a function that draws an ivisiable grid that is placed on the inside of the game board and sets boarders.
 };
